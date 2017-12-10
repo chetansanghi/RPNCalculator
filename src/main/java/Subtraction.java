bbf3e23f60ad56a0ce02f42c9ecbfe811af9c7ad
chourasiav268 <vishal.chourasia@worldpay.com>
2017-12-10 12:30:53 +0000

    Test for rebase

--- src/main/java/Subtraction.java
+++ src/main/java/Subtraction.java
@@ -5,6 +5,8 @@ import main.java.RPN.Operations;
 public class Subtraction extends Operations{
 
 	@Override
+	
+	//Method to subtract
 	public Double calculate(Double operand1, Double operand2) throws Exception {
 		if(operand1 != null && operand2 != null){
 			return (operand2 - operand1);
