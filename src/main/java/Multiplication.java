da97543833b16d5cbbd28c2716d792ac9209072d
chourasiav268 <vishal.chourasia@worldpay.com>
2017-12-10 12:30:53 +0000

    Test for rebase

--- src/main/java/Multiplication.java
+++ src/main/java/Multiplication.java
@@ -5,6 +5,8 @@ import main.java.RPN.Operations;
 public class Multiplication extends Operations{
 
 	@Override
+
+	//Method to multiplicate 2 given numbers
 	public Double calculate(Double operand1, Double operand2) {
 		if(operand1 != null && operand2 != null){
 			return (operand1 * operand2);
